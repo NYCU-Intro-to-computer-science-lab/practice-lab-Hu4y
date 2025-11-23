049473333f4ca33fc52b8f03cf685da361b9c057
Xian <xzsawq0610@gmail.com>
2025-11-23 18:11:47 +0800

    GUI 自動提交

--- src/p1.cpp
+++ src/p1.cpp
@@ -7,12 +7,27 @@ using namespace std;
 // 回傳: 根據 opcode 計算的結果
 int calculate(int opcode, int num1, int num2) {
     // 在此實作你的程式碼
+    if(opcode == 1) {
+        return (num1 + num2);
+    }
+    else if(opcode == 2) {
+        return (num1 - num2);
+    }
+    else if(opcode == 3) {
+        return (num1 * num2);
+    }
+    else
+        return (num1 / num2);
     return 0;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int opcode, num1, num2;
+    while(cin >> opcode >> num1 >> num2) {
+        cout << calculate(opcode, num1, num2) << endl;
+    }
     
     // TODO: 讀取輸入並呼叫 calculate 函數
     // 提示: 使用 while(cin >> opcode >> num1 >> num2) 來讀取到 EOF
--- src/p3.cpp
+++ src/p3.cpp
@@ -9,13 +9,18 @@ using namespace std;
 //       Recursive case: climb(n) = climb(n-1) + climb(n-2)
 int climb(int n) {
     // 在此實作你的程式碼
-    return 0;
+    if(n == 0 || n == 1)
+    return 1;
+    else
+    return climb(n-1) + climb(n-2);
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // TODO: 讀取 n 並呼叫 climb 函數，輸出結果
-    
+    int n;
+    cin >> n;
+    cout << climb(n) << endl;
     return 0;
 }
